b4989fd2cd84d26c7368a7e246c222a18dcba01f
Richard Miller-Smith <richard@darwin-innovation.com>
2013-10-25 17:37:57 +0100

    add ^C handling

--- test/test1.py
+++ test/test1.py
@@ -6,6 +6,9 @@ Created on 25 Oct 2013
 
 from DarwinVets.Bloods.SpotChem import SpotChem
 from DarwinVets.Bloods.Mythic import Mythic
+import signal
+import sys
+
 
 if __name__ == '__main__':
     mythic = Mythic("/dev/ttyUSB0", 9600)
@@ -13,4 +16,15 @@ if __name__ == '__main__':
     
     mythic.start()
     spotchem.start()
+
+    def sigint_handler(sig, frame):
+        print 'You pressed Ctrl+C - exiting'
+        mythic.exit()
+        spotchem.exit()
+        sys.exit(0)
+    
+    signal.signal(signal.SIGINT, sigint_handler)
+    
+    mythic.join()
+    spotchem.join()
     
