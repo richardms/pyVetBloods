9cc8400fe541eb80e2f9ca66c49ac69fa507f88e
Richard Miller-Smith <richard@darwin-innovation.com>
2013-10-29 14:06:18 +0000

    Fix Result inst

--- DarwinVets/Bloods/SpotChem.py
+++ DarwinVets/Bloods/SpotChem.py
@@ -59,7 +59,7 @@ class SpotChem(BloodAnalyserBase):
             self.debug("Block '%s'", block)
             
             if self._result is None:
-                self._result = Result()
+                self._result = Result.Result()
             
             send = self._handleBlock_fmt2(block, endchar)
             if send:
